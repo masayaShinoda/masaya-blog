e4cd221533aa77e10869b27c5fe5e43dc2a2f73d
masayaShinoda <masayashida@gmail.com>
2020-04-24 13:17:42 +0700

    homepage update and minor style changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,41 +6,35 @@ import SEO from "../components/seo"
 
 //styles import
 import aboutStyles from "../styles/styles.css"
-//icons import
-import homeIcon from "../assets/home-icon.png"
-import podcastsIcon from "../assets/podcasts-icon.png"
-import blogIcon from "../assets/blog-icon.png"
 
 const AboutPage = () => {
   return (
     <Layout>
       <SEO title="About" keywords={[`about`]} />
-      <div className="pageContainer">
-        <div className="aboutContainer">
-          <div className="flexAbout">
-            <h1>About</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
-          </div>
-          <div className="flexContact">
-            <h2>Contact:</h2>
-            <a
-              href="mailto:info@breaddesignstudio.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa fa-envelope"></i>
-              <p>masayashida@gmail.com</p>
-            </a>
-            <span>
-              <i className="fa fa-map-pin"></i>
-              <p>Phnom Penh, Cambodia.</p>
-            </span>
-          </div>
+      <div className="aboutContainer">
+        <div className="flexAbout">
+          <h1>About</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+        <div className="flexContact">
+          <h2>Contact:</h2>
+          <a
+            href="mailto:info@breaddesignstudio.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa fa-envelope"></i>
+            <p>masayashida@gmail.com</p>
+          </a>
+          <span>
+            <i className="fa fa-map-pin"></i>
+            <p>Phnom Penh, Cambodia.</p>
+          </span>
         </div>
       </div>
     </Layout>
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,24 @@ import indexStyles from "../styles/styles.css"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO
+      title="Home"
+      keywords={[
+        `home`,
+        `masaya`,
+        `masaya shida`,
+        `blog`,
+        `masayablog`,
+        `masaya's blog`,
+      ]}
+    />
     <div className="pageContainer">
       <section>
         <h1>Welcome to my site!</h1>
+        <p>
+          Here is where I articulate and share my thoughts. Hope you find
+          something interesting to <Link to="/blog">read</Link> &#128516;.{" "}
+        </p>
       </section>
     </div>
   </Layout>
