6ad6f99fb9b187986dfc915c6f0854e6888bf2ad
masayaShinoda <masayashida@gmail.com>
2020-06-23 13:31:40 +0700

    added twitter card meta tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,18 @@ const IndexPage = () => (
         property="og:image"
         content="https://raw.githubusercontent.com/masayaShinoda/photography/master/src/assets/images/angkor.jpg"
       />
-
+      {/* <!-- Twitter --> */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://metatags.io/" />
+      <meta property="twitter:title" content="Masaya's Blog" />
+      <meta
+        property="twitter:description"
+        content="Here is where I articulate and share my thoughts. Hope you find something interesting to read while you're here 😄."
+      />
+      <meta
+        property="twitter:image"
+        content="https://raw.githubusercontent.com/masayaShinoda/photography/master/src/assets/images/fulls/01.jpg"
+      ></meta>
       <meta
         name="google-site-verification"
         content="oGtdccjbXYieyxzBCZTLAhlNJh9Cbbpr82fUirt1lHc"
@@ -51,9 +62,9 @@ const IndexPage = () => (
       <section>
         <h1>Welcome!</h1>
         <p>
-          Here is where I learn to articulate and share my thoughts. Hope you find
-          something interesting to <Link to="/blog">read</Link> while you're
-          here 😄.{" "}
+          Here is where I learn to articulate and share my thoughts. Hope you
+          find something interesting to <Link to="/blog">read</Link> while
+          you're here 😄.{" "}
         </p>
         <p>
           Are you into photography? Give this other{" "}
